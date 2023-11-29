712c819ce01ebec3e9bc6282aeab662c3c5a11ba
bi0punk <systecinfo_cl@criptext.com>
2023-11-29 01:22:34 -0300

    add new words

--- app.py
+++ app.py
@@ -10,38 +10,34 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 ruta_archivo_log = '/root/server/logs/latest.log'  # Reemplaza con la ruta correcta
-last_sent_message = None
+historial_mensajes = set()
 
 async def send_auto_message():
-    global last_sent_message  # Asegura que estás utilizando la variable global
-    
     channel_id = 1103919972771708949  # Reemplaza con el ID del canal
     channel = bot.get_channel(channel_id)
 
     while True:
-        await asyncio.sleep(30)  # Espera 60 segundos antes de verificar el archivo
+        await asyncio.sleep(30)  # Espera 30 segundos antes de verificar el archivo
 
         # Verifica si hay nuevos mensajes en el archivo de registro relacionados con jugadores
         nuevos_mensajes = obtener_nuevos_mensajes(ruta_archivo_log)
         for mensaje in nuevos_mensajes:
-            # Verifica si el mensaje es igual al último mensaje enviado
-            if mensaje != last_sent_message:
+            # Verifica si el mensaje ya ha sido enviado antes
+            if mensaje not in historial_mensajes:
                 await enviar_mensaje(channel, mensaje)
-                last_sent_message = mensaje
+                historial_mensajes.add(mensaje)
 
 class ManejadorDeEventos(FileSystemEventHandler):
     async def on_modified(self, event):
-        global last_sent_message  # Asegura que estás utilizando la variable global
-        
-        if event.src_path == ruta_archivo_log:
-            nuevos_mensajes = obtener_nuevos_mensajes(ruta_archivo_log)
-            for mensaje in nuevos_mensajes:
-                channel_id = TU_ID_DE_CANAL  # Reemplaza con el ID del canal
-                channel = bot.get_channel(channel_id)
+        nuevos_mensajes = obtener_nuevos_mensajes(ruta_archivo_log)
+        for mensaje in nuevos_mensajes:
+            channel_id = TU_ID_DE_CANAL  # Reemplaza con el ID del canal
+            channel = bot.get_channel(channel_id)
             
-                if mensaje != last_sent_message:
-                    await enviar_mensaje(channel, mensaje)
-                    last_sent_message = mensaje
+            # Verifica si el mensaje ya ha sido enviado antes
+            if mensaje not in historial_mensajes:
+                await enviar_mensaje(channel, mensaje)
+                historial_mensajes.add(mensaje)
 
 def obtener_nuevos_mensajes(ruta):
     palabras_clave = ["joined the game", 
@@ -50,7 +46,9 @@ def obtener_nuevos_mensajes(ruta):
                     "left the game", 
                     "has made the advancement", 
                     "fell from a high place", 
-                    "drowned"]
+                    "drowned",
+                    "tried to swim in lava"
+                    ]
 
     with open(ruta, 'r') as archivo:
         lineas = archivo.readlines()
