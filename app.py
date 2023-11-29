d9f6f255d31a73444e262167fc2b02c51a654289
bi0punk <systecinfo_cl@criptext.com>
2023-11-29 00:47:01 -0300

    add control

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ ruta_archivo_log = '/root/server/logs/latest.log'  # Reemplaza con la ruta corre
 last_sent_message = None
 
 async def send_auto_message():
+    global last_sent_message  # Asegura que estás utilizando la variable global
+    
     channel_id = 1103919972771708949  # Reemplaza con el ID del canal
     channel = bot.get_channel(channel_id)
 
@@ -29,14 +31,14 @@ async def send_auto_message():
 
 class ManejadorDeEventos(FileSystemEventHandler):
     async def on_modified(self, event):
+        global last_sent_message  # Asegura que estás utilizando la variable global
+        
         if event.src_path == ruta_archivo_log:
-            # Verifica si hay nuevos mensajes en el archivo de registro relacionados con jugadores
             nuevos_mensajes = obtener_nuevos_mensajes(ruta_archivo_log)
             for mensaje in nuevos_mensajes:
                 channel_id = TU_ID_DE_CANAL  # Reemplaza con el ID del canal
                 channel = bot.get_channel(channel_id)
-                
-                # Verifica si el mensaje es igual al último mensaje enviado
+            
                 if mensaje != last_sent_message:
                     await enviar_mensaje(channel, mensaje)
                     last_sent_message = mensaje
@@ -44,12 +46,10 @@ class ManejadorDeEventos(FileSystemEventHandler):
 def obtener_nuevos_mensajes(ruta):
     with open(ruta, 'r') as archivo:
         lineas = archivo.readlines()
-        
-        # Filtra las líneas que contienen información sobre la entrada, derrota, pérdida de conexión o salida de jugadores
+    
         mensajes_filtrados = [linea.strip() for linea in lineas if "joined the game" in linea or "was slain by" in linea or "lost connection" in linea or "left the game" in linea or "has made the advancement" in linea]
 
         return mensajes_filtrados
-
 async def enviar_mensaje(channel, contenido):
     embed = discord.Embed(
         title="Nuevo mensaje del servidor de Minecraft",
@@ -57,7 +57,6 @@ async def enviar_mensaje(channel, contenido):
         color=0x00b0f4,
         timestamp=datetime.now()
     )
-
     embed.set_author(name="Info Minecraft World")
     embed.set_footer(icon_url="https://slate.dan.onl/slate.png")
 
@@ -67,12 +66,10 @@ async def enviar_mensaje(channel, contenido):
 async def on_ready():
     print(f'Bot conectado como {bot.user.name}')
     
-    # Inicializa el observador y lo asocia con la clase de manejo de eventos
     observador = Observer()
     observador.schedule(ManejadorDeEventos(), path=ruta_archivo_log)
     observador.start()
 
-    # Inicia la tarea para enviar mensajes automáticos
     bot.loop.create_task(send_auto_message())
 
 bot.run('')  # Coloca tu token aquí
