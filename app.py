de2df11456702b788db65468a579ec06e87a8ecd
bi0punk <systecinfo_cl@criptext.com>
2023-11-29 00:15:41 -0300

    add auto message

--- app.py
+++ app.py
@@ -18,30 +18,31 @@ async def send_auto_message():
     while True:
         await asyncio.sleep(60)  # Espera 60 segundos antes de verificar el archivo
 
-        # Verifica si hay un nuevo mensaje en el archivo de registro
-        nuevo_mensaje = obtener_ultimo_mensaje(ruta_archivo_log)
-        if nuevo_mensaje:
-            await enviar_ultimo_mensaje(channel, nuevo_mensaje)
+        # Verifica si hay nuevos mensajes en el archivo de registro relacionados con jugadores
+        nuevos_mensajes = obtener_nuevos_mensajes(ruta_archivo_log)
+        for mensaje in nuevos_mensajes:
+            await enviar_mensaje(channel, mensaje)
 
 class ManejadorDeEventos(FileSystemEventHandler):
     async def on_modified(self, event):
         if event.src_path == ruta_archivo_log:
-            # Verifica si hay un nuevo mensaje en el archivo de registro
-            nuevo_mensaje = obtener_ultimo_mensaje(ruta_archivo_log)
-            if nuevo_mensaje:
+            # Verifica si hay nuevos mensajes en el archivo de registro relacionados con jugadores
+            nuevos_mensajes = obtener_nuevos_mensajes(ruta_archivo_log)
+            for mensaje in nuevos_mensajes:
                 channel_id = TU_ID_DE_CANAL  # Reemplaza con el ID del canal
                 channel = bot.get_channel(channel_id)
-                await enviar_ultimo_mensaje(channel, nuevo_mensaje)
+                await enviar_mensaje(channel, mensaje)
 
-def obtener_ultimo_mensaje(ruta):
+def obtener_nuevos_mensajes(ruta):
     with open(ruta, 'r') as archivo:
         lineas = archivo.readlines()
-        if lineas:
-            return lineas[-1].strip()
-        else:
-            return None
+        
+        # Filtra las líneas que contienen información sobre la entrada, derrota, pérdida de conexión o salida de jugadores
+        mensajes_filtrados = [linea.strip() for linea in lineas if "joined the game" in linea or "was slain by" in linea or "lost connection" in linea or "left the game" in linea]
 
-async def enviar_ultimo_mensaje(channel, contenido):
+        return mensajes_filtrados
+
+async def enviar_mensaje(channel, contenido):
     embed = discord.Embed(
         title="Nuevo mensaje del servidor de Minecraft",
         description=f"```\n{contenido}\n```",
