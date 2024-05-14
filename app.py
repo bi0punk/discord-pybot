d412e828388e7e23bc37f89d6f3de6136616fb7c
sysbot <sysbot@kali>
2024-05-14 00:58:36 -0400

    fix fraccio problems

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ from discord.ext import commands
 from datetime import datetime
 from fractions import Fraction
 
-
-
-
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -19,13 +16,18 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 def evaluar_expresion_matematica(expresion):
+    # Reemplazamos fracciones de la forma 'a/b' por 'Fraction(a, b)'
     expresion = re.sub(r'(\d+)/(\d+)', r'Fraction(\1, \2)', expresion)
     print("Expresión evaluada:", expresion)
     
-    # Modificamos el patrón de la expresión regular para permitir fracciones
-    if re.match(r"^[\d()+\-*/\sFractn]+$", expresion):  # Agregamos "Fractn" al patrón para permitir fracciones
+    # Verificamos explícitamente la división por cero
+    if re.search(r'Fraction\(\d+, 0\)', expresion) or re.search(r'/\s*0', expresion):
+        return "División por cero, en este universo, No Existe.", None
+ 
+    if re.match(r"^[\d()+\-*/\sFraction,]+$", expresion):  
         try:
             inicio = datetime.now()
+            # Evaluamos la expresión en un entorno seguro
             resultado = eval(expresion, {"Fraction": Fraction})
             fin = datetime.now()
             tiempo_ejecucion = (fin - inicio).total_seconds()
@@ -33,27 +35,22 @@ def evaluar_expresion_matematica(expresion):
         except Exception as e:
             return f"Error al evaluar la expresión: {e}", None
     else:
-        return None, None
+        return "La expresión no es válida.", None
 
-
-
-    
 @bot.command()
 async def calcular(ctx, *, expresion):
     resultado, tiempo_ejecucion = evaluar_expresion_matematica(expresion)
     if resultado:
-        embed = discord.Embed(title="Calculator",
+        embed = discord.Embed(title="Calculadora",
                               description=f"```md\n# Operación\n{expresion}\n\n# Resultado\n{resultado}\n```",
                               colour=0x00b0f4,
                               timestamp=datetime.now())
-        embed.set_author(name="Math")
+        #embed.set_author(name="Math Bot")
         embed.set_footer(text=f"Tiempo de ejecución: {tiempo_ejecucion} segundos",
                          icon_url="https://slate.dan.onl/slate.png")
         await ctx.send(embed=embed)
     else:
         await ctx.send("La expresión no es válida.")
-
-
 
 @bot.event
 async def on_ready():
